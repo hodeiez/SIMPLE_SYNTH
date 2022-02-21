c5b72ac71f9a40a3229e7f110fbf73ed7fcd79e8
hodeiez <hodeinaiz@hotmail.com>
2022-02-21 21:51:40 +0100

    fixed main

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	/* "log" */
 
+	/* "github.com/go-audio/transforms" */
 	"hodei.naiz/simplesynth/synth/dsp"
 	"hodei.naiz/simplesynth/synth/generator"
 	"hodei.naiz/simplesynth/synth/midi"
@@ -11,14 +13,15 @@ import (
 func main() {
 	fmt.Println("hello synth")
 	bufferSize := 512
-
 	osc := generator.Oscillator(bufferSize)
-	//	note := make(chan int)
 	message := midi.MidiMsg{Key: 0, On: false}
 	go func() {
+
 		midi.RunMidi(&message)
+
 	}()
 	go func() {
+
 		start := dsp.DspConf{BufferSize: bufferSize, Osc: osc}
 		dsp.Run(start)
 	}()
