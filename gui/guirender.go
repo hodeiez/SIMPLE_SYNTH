90155863f8c29d13c32b8abf615bd3a3caa4ecc7
hodeiez <hodeinaiz@hotmail.com>
2022-03-08 00:05:51 +0100

    added adsr image

--- gui/guirender.go
+++ gui/guirender.go
@@ -1,10 +1,14 @@
 package gui
 
 import (
+	"image"
 	"image/color"
 	"strconv"
 
 	"gioui.org/app"
+	"gioui.org/f32"
+	"gioui.org/op/clip"
+	"gioui.org/op/paint"
 
 	"gioui.org/font/gofont"
 	"gioui.org/io/system"
@@ -48,7 +52,9 @@ func Render(w *app.Window, controller *generator.Controls) error {
 			gtx := layout.NewContext(&ops, e)
 			layout.E.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 				return marginCenter.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+
 					return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceEvenly}.Layout(gtx,
+
 						layout.Rigid(func(gtx layout.Context) layout.Dimensions {
 
 							return material.Body2(th, strconv.FormatFloat(float64(sliders[0].FloatWidget.Value), 'f', 2, 64)).Layout(gtx)
@@ -62,10 +68,22 @@ func Render(w *app.Window, controller *generator.Controls) error {
 
 				})
 			})
+			layout.NE.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
+				return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceBetween}.Layout(gtx,
+					layout.Rigid(
+						func(gtx layout.Context) layout.Dimensions {
+							circle := clip.Ellipse{f32.Point{sliders[2].FloatWidget.Value, sliders[3].FloatWidget.Value}, f32.Point{-1 * sliders[0].FloatWidget.Value, sliders[1].FloatWidget.Value}}.Op(gtx.Ops)
+							color := color.NRGBA{R: 255, A: 255, B: 100}
+							paint.FillShape(gtx.Ops, color, circle)
+							d := image.Point{Y: 500}
+							return layout.Dimensions{Size: d}
+						}),
+				)
+			})
+
 			layout.SW.Layout(gtx, func(gtx layout.Context) layout.Dimensions {
 				return layout.Flex{Axis: layout.Horizontal, Spacing: layout.SpaceBetween}.Layout(gtx,
 					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
-
 						return material.Label(th, unit.Dp(20), "THE SIMPLE SYNTH").Layout(gtx)
 					}),
 					layout.Rigid(func(gtx layout.Context) layout.Dimensions {
@@ -88,4 +106,19 @@ func bindControls(controller *generator.ADSRControl, sliders []components.MySlid
 	*controller.DecayTime = float64(sliders[1].FloatWidget.Value)
 	*controller.SustainAmp = float64(sliders[2].FloatWidget.Value)
 	*controller.ReleaseTime = float64(sliders[3].FloatWidget.Value)
+}
+func redTriangle(ops *op.Ops) {
+	var path clip.Path
+	path.Begin(ops)
+	path.Move(f32.Pt(50, 0))
+	path.Quad(f32.Pt(0, 90), f32.Pt(50, 100))
+	path.Line(f32.Pt(-100, 0))
+	path.Line(f32.Pt(50, -100))
+	defer clip.Outline{Path: path.End()}.Op().Push(ops).Pop()
+	drawRedRect(ops)
+}
+func drawRedRect(ops *op.Ops) {
+	defer clip.Rect{Max: image.Pt(100, 100)}.Push(ops).Pop()
+	paint.ColorOp{Color: color.NRGBA{R: 0x80, A: 0xFF}}.Add(ops)
+	paint.PaintOp{}.Add(ops)
 }
